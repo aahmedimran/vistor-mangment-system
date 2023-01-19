b7c6bda8fecf9b1515121994490b98abdc58b5a6
ahmed imran <ahmed.eng2709@gmail.com>
2023-01-19 23:26:15 +0500

    all deportment of admin show user vistor page

--- vms web/src/pages/Uservistor/index.jsx
+++ vms web/src/pages/Uservistor/index.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import axios from 'axios'
+import moment from 'moment'
+import React,{useState,useEffect,useContext} from 'react'
+import Deportmentpagination from '../../component/Deportment/Deportmentpagination'
 import UservistorCompfrom from '../../component/Deportment/userVistor'
+import { GlobalContext } from '../../Context/context'
 
 const Uservistor = () => {
+const {state} = useContext(GlobalContext)
+
+const [getDeportments, setgetDeportments] = useState([])
+
+ // filterData
+ const [filterData] = useState("");
+// use for pagination
+const [currantPage, setcurrantPage] = useState(1)
+const [postPerpage] = useState(5)
+
+ // Get currant page
+
+ const indexOfLastPost = currantPage * postPerpage;
+ const indexOfFirstPost = indexOfLastPost - postPerpage;
+ const currantPost = getDeportments.slice(indexOfFirstPost, indexOfLastPost);
+
+
+
+useEffect(() => {
+  let getDeportments = async ()=>{
+try{
+    let response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/GetDeportmentspacific/${state.user.adminId}`, { withCredentials: true })
+if(response.status === 200){
+    console.log(response.data,"response")
+    setgetDeportments(response.data.data.reverse())
+}
+}
+catch(e){
+    console.log(e,"error on api call")
+}
+
+
+  }
+
+  getDeportments()
+
+}, [state.user.adminId])
+
+
+
+
+
     return (
         <>
             <UservistorCompfrom />
@@ -35,6 +81,56 @@ const Uservistor = () => {
                         <input type="text" placeholder='search' />
                     </div>
                 </div>
+
+
+                <div className='table-main'>
+        <table id='content'>
+          <tbody >
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Creeated On</th>
+              <th>Updated By</th>
+              <th>Action</th>
+            </tr>
+            {/* all add on condinoly  */}
+            {currantPost.filter(
+              (deportment) =>
+              deportment.deportmentName.toLowerCase().includes(filterData)
+              
+            )
+              .map((deportment) => (
+
+                <tr key={deportment._id}>
+                  <td>{deportment?.key}</td>
+                  <td>{deportment?.deportmentName}</td>
+                  <td>{deportment?.contactPerson}</td>
+                  <td> {moment(deportment.createdAt).format('l LT')}</td>
+                  <td>{moment(deportment.updatedAt).format('L LT')}</td>
+                  <td>  <button type='submit'  >Edit</button>
+                    <button type='submit'>Delate</button></td>
+                </tr>
+              )
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+
+
+      <Deportmentpagination postPerpage={postPerpage} totalPosts={getDeportments.length}
+        setcurrantPage={setcurrantPage} />
+
+
+
+
+
+
+
+
+
+
                 <div>
                     <table id='content'>
                         <tbody >
@@ -64,6 +160,10 @@ const Uservistor = () => {
                         </tbody>
                     </table>
                 </div>
+
+
+
+
             </div>
         </>
     )
