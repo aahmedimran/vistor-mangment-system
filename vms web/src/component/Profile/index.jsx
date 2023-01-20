017aa1ea9e2f827cf105f68c71e12eef3cbc39f3
ahmed imran <ahmed.eng2709@gmail.com>
2023-01-20 12:05:04 +0500

    set updated data on state

--- vms web/src/component/Profile/index.jsx
+++ vms web/src/component/Profile/index.jsx
@@ -1,29 +1,27 @@
-import React, { useState, useContext,useEffect } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { GlobalContext } from '../../Context/context'
 import { HashLoader } from "react-spinners";
 import axios from 'axios'
 import './index.css'
 const Profile = () => {
-  const { state } = useContext(GlobalContext)
+  const { state,dispatch } = useContext(GlobalContext)
   const [firstName, setfirstName] = useState('')
   const [lastName, setlastName] = useState('')
   const [email, setemail] = useState('')
   const [validFirstName, setValidFirstName] = useState(false)
   const [validLastName, setValidLastName] = useState(false)
   const apiUrl = process.env.REACT_APP_BASE_URL;
-const [loading, setloading] = useState(true)
+  const [loading, setloading] = useState(true)
 
-useEffect(() => {
-  setfirstName(state?.user?.firstName);
-  setlastName(state?.user?.lastName);
-  setemail(state?.user?.email);
-  
-  setTimeout(() => {
-    setloading(false)
-  }, 500);
-}, [state?.user])
+  useEffect(() => {
+    setfirstName(state?.user?.firstName);
+    setlastName(state?.user?.lastName);
+    setemail(state?.user?.email);
 
-
+    setTimeout(() => {
+      setloading(false)
+    }, 500);
+  }, [state?.user])
 
   const profileUpdateHandler = async () => {
 
@@ -40,12 +38,12 @@ useEffect(() => {
       return
     }
     try {
-      let response = await axios.put(`${apiUrl}/api/Profile/${state?.user?._id}`, {
+      let response = await axios.put(`${apiUrl}/api/Update/${state?.user?._id}`, {
 
         firstName: firstName,
         lastName: lastName
       }, { withCredentials: true })
-
+      dispatch({ type: "USER_LOGIN", payload: response.data.data });
       console.log(response, "response")
 
     }
@@ -54,51 +52,51 @@ useEffect(() => {
     }
 
   }
-  
+
   return (
     <>
-{ loading ?
+      {loading ?
 
-<div className='loader'>
-<HashLoader
-  color={"#13F513"}
-  loading={loading}
+        <div className='loader'>
+          <HashLoader
+            color={"#13F513"}
+            loading={loading}
 
-  size={150}
-  aria-label="Loading Spinner"
-  data-testid="loader"
-/>
-</div>
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
 
-:
-<div className='profile-container' >
-        <div className='profile-main'>
-          <div className='profile-from'>
-            <h1>User Profile</h1>
-            <div className='profile-inputs'>
-              <label htmlFor="">First Name</label>
-              <input type="text" placeholder='First Name' value={firstName} onChange={(e) => { setfirstName(e.target.value) }} />
+        :
+        <div className='profile-container' >
+          <div className='profile-main'>
+            <div className='profile-from'>
+              <h1>User Profile</h1>
+              <div className='profile-inputs'>
+                <label htmlFor="">First Name</label>
+                <input type="text" placeholder='First Name' value={firstName} onChange={(e) => { setfirstName(e.target.value) }} />
+              </div>
+              <div className='subuser-input-usererror'>
+                {validFirstName === true ? <span>Please enter Name</span> : <p></p>}
+              </div>
+              <div className='profile-inputs'>
+                <label htmlFor="">Last name</label>
+                <input type="text" placeholder='Last Name' value={lastName} onChange={(e) => { setlastName(e.target.value) }} />
+              </div>
+              <div className='subuser-input-usererror'>
+                {validLastName === true ? <span>Please enter Name</span> : <p></p>}
+              </div>
+              <div className='profile-inputs'>
+                <label htmlFor="">Email</label>
+                <input type="text" placeholder='Email' disabled value={email} onChange={(e) => { setemail(e.target.value) }} />
+              </div>
+              <button type='submit' className='profile-btn' onClick={profileUpdateHandler} >update</button>
             </div>
-            <div className='subuser-input-usererror'>
-              {validFirstName === true ? <span>Please enter Name</span> : <p></p>}
-            </div>
-            <div className='profile-inputs'>
-              <label htmlFor="">Last name</label>
-              <input type="text" placeholder='Last Name' value={lastName} onChange={(e) => { setlastName(e.target.value) }} />
-            </div>
-            <div className='subuser-input-usererror'>
-              {validLastName === true ? <span>Please enter Name</span> : <p></p>}
-            </div>
-            <div className='profile-inputs'>
-              <label htmlFor="">Email</label>
-              <input type="text" placeholder='Email' disabled value={email} onChange={(e) => { setemail(e.target.value) }} />
-            </div>
-            <button type='submit' className='profile-btn' onClick={profileUpdateHandler} >update</button>
           </div>
         </div>
-      </div>
 
-}
+      }
 
       {/* <div className='profile-container' >
         <div className='profile-main'>
