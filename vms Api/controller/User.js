dca33d73985325ed8bf39053534121bd6536da71
ahmed imran <ahmed.eng2709@gmail.com>
2023-01-22 00:50:57 +0500

    create verfy email api click on link

--- vms Api/controller/User.js
+++ vms Api/controller/User.js
@@ -1,7 +1,6 @@
 const userModel = require("../models/user.model")
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
-
 const nodemailer = require("nodemailer")
 const User = {
 
