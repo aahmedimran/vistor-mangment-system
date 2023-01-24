19e2bd443537b677b56b4aa3898f156a45bd1ed5
ahmed imran <ahmed.eng2709@gmail.com>
2023-01-24 15:15:58 +0500

    display all vistor data

--- vms web/src/component/Vistor/index.jsx
+++ vms web/src/component/Vistor/index.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 import './index.css'
+import axios from 'axios'
+import Deportmentpagination from '../Deportment/Deportmentpagination'
 const Vistor = () => {
+  const [getVistors, setgetVistors] = useState([])
+  // filterData
+  const [filterData] = useState("");
+  // use for pagination
+  const [currantPage, setcurrantPage] = useState(1)
+  const [postPerpage] = useState(5)
+
+  // Get currant page
+
+  const indexOfLastPost = currantPage * postPerpage;
+  const indexOfFirstPost = indexOfLastPost - postPerpage;
+  const currantPost = getVistors.slice(indexOfFirstPost, indexOfLastPost);
+
+
+  useEffect(() => {
+
+    const getVistor =async () => {
+
+      try {
+        let response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api//Getvistor`, { withCredentials: true })
+
+        if (response.status === 200) {
+          // console.log("response : ", response.data);
+
+          // setloading(false)
+
+          setgetVistors(response.data.data.reverse())
+
+        } else {
+          console.log("Error in api call: ");
+        }
+      }
+      catch (e) {
+
+        console.log(e, "error")
+      }
+
+
+    }
+    getVistor()
+
+
+  }, [])
 
 
 
@@ -43,36 +89,54 @@ const Vistor = () => {
             <input type="text" placeholder='search' />
           </div>
         </div>
-        <div>
-          <table id='content'>
-            <tbody >
-              <tr>
-                <th>Vistor Name</th>
-                <th>Meet Person</th>
-                <th>Deportment</th>
-                <th>In Date</th>
-                <th>Out Date</th>
-                <th>Status</th>
-                <th>Enter by</th>
-                <th>Action</th>
-              </tr>
-
-
-              <tr>
-                <td>sunny</td>
-                <td>shayan</td>
-                <td> It</td>
-                <td>22</td>
-                <td>22</td>
-                <td><button>true</button></td>
-                <td>sunny</td>
-                <td><button>view</button><button>del</button></td>
-              </tr>
-
-            </tbody>
-          </table>
-        </div>
       </div>
+      <div className='table-main'>
+        <table id='content'>
+          <tbody >
+            <tr>
+              <th>Vistor Name</th>
+              <th>Meet Person</th>
+              <th>Deportment</th>
+              <th>In Date</th>
+              <th>Out Date</th>
+              <th>Status</th>
+              <th>Enter by</th>
+              <th>Action</th>
+            </tr>
+            {/* all add on condinoly  */}
+            {currantPost.filter(
+              (vistor) =>
+                vistor.deportment.toLowerCase().includes(filterData)
+                || vistor.vistorName.toLowerCase().includes(filterData)
+                || vistor.meetPersonName.toLowerCase().includes(filterData)
+            )
+              .map((vistor) => (
+                <tr key={vistor._id}>
+                  <td>{vistor?.vistorName}</td>
+                  <td>{vistor?.meetPersonName}</td>
+                  <td>{vistor?.deportment}</td>
+                  <td>{moment(vistor?.createdAt).format('l LT')}</td>
+
+                 { console.log(vistor.status,"vistor🚗🚗🚗🚗🚗🚗🚗")}
+
+
+
+                  <td> {moment(vistor?.updatedAt).format('l LT')}</td>
+                  <td style={{  backgroundColor: 'white',color: 'red'}}>{vistor.status}</td>
+                  <td>{vistor?.enterBy} </td>
+                  <td>  <button type='submit'  >Edit</button>
+                    <button type='submit'>Delate</button></td>
+                </tr>
+              )
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+      <Deportmentpagination postPerpage={postPerpage} totalPosts={getVistors.length}
+        setcurrantPage={setcurrantPage} />
+
+
 
     </>
   )
